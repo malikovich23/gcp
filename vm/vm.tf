6ec66a8ffb00b05cadf9ed8eadfe26345df7a384
malikovich23 <malikovich23@outlook.com>
2022-05-31 18:42:32 +0000

    added vm fw

---
+++ vm/vm.tf
@@ -0,0 +1,16 @@
+resource "google.com_compute_instance" "vm_instance" {
+    name = var.vm_config["instance_name"]
+    machine_type = var.vm_config["machine_type"]
+    boot_disk {
+        initialize_params {
+            image - var.vm_config["image"]
+        }
+    }
+    network_interface {
+        network = "default"
+        access_config {
+        }
+    }
+    labels = var.labels
+    tags = [var.vm_config["network_tags"]]
+}
