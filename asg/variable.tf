f6e0f6ed4b96f55cf059323172ffd2618295b140
EC2 Default User <malikovich23@outlook.com>
2022-06-01 22:16:12 +0000

    added new files asg provider variable

---
+++ asg/variable.tf
@@ -0,0 +1,18 @@
+variable "asg_config" {
+    type = map(any)
+    default = {
+        project = "iaac-351916"
+        region = "us-central1"
+        zone = "us-central1-c"
+        target-pool-name = "my-target-pool"
+        autoscaler = "my-autoscaler"
+        max_replicas = 5
+        min_replicas = 1
+        cooldown_period = 60
+        target = 0.5
+        instance_group_manager_name = "my-igm"
+        instance_template_name = "my-instance-template"
+        machine_type = "e2-medium"
+        source_image = "debian-cloud/debian-9"
+    }
+}
